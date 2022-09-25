03de1cdfdb757534a6aeadcdcb1573f68295a082
Chanmaru01 <chanmaru1336@gmail.com>
2022-09-25 10:34:47 +0900

    カラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users/show" => "users#show"
 
   root to: "home#index"
 end
