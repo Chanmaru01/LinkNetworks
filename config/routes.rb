61c0725db71428e31d50e7ae42ab10091a6f060f
Chanmaru01 <chanmaru1336@gmail.com>
2022-10-06 18:52:07 +0900

    エラー部分

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "users/show" => "users#show"
 
+  get 'tweets/index' => 'tweets#index'
+  get 'tweets/:id' => 'tweets#show'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
+  patch 'tweets/:id' => 'tweets#update'
+  delete 'tweets/:id' => 'tweets#destroy'
+  get 'tweets/:id/edit' => 'tweets#edit'
+  
+  root to: "tweets#index"
   root to: "home#index"
 end
