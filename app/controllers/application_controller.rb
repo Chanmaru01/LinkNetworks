07a1529adb383806a3024f647b25822c1061cd13
Chanmaru01 <chanmaru1336@gmail.com>
2023-03-15 05:06:57 +0900

    View修正--4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  # ログイン済ユーザーのみにアクセスを許可する
-  before_action :authenticate_user!
   
   # deviseコントローラーにストロングパラメータを追加する          
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # ログイン済ユーザーのみにアクセスを許可する
+  # before_action :authenticate_user!, except: [:index]
 
   protected
   def configure_permitted_parameters
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
     # ログインしていない状態だとログイン画面に飛ばされる
-    # skip_before_action :authenticate_user!
+    # skip_before_action :authenticate_user!, except: [:index]
+    # before_action :authenticate_user!, only: [:index]
 
     def index
         redirect_to tweets_path if user_signed_in?
     end
+    
 end
